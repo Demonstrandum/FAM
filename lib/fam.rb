32dd472e3c97fab8b1e3c69b092832e8a4bb1a39
Demonstrandum <knutsen@jetspace.co>
2018-04-04 20:00:24 +0100

    Bump version.

--- lib/fam.rb
+++ lib/fam.rb
@@ -5,7 +5,7 @@ class Object
 end
 
 module FAM
-  VERSIONS = { :major => 0, :minor => 0, :tiny => 2 }
+  VERSIONS = { :major => 0, :minor => 1, :tiny => 0 }
 
   def self.version *args
     VERSIONS.flatten.select.with_index { |val, i| i.odd? }.join '.'
